8c0b8c44d7d32fbd2519a5b855b7110a51a2e6f7
Celvis <78430018+Celvis-wq@users.noreply.github.com>
2023-01-20 20:47:36 -0600

    Data was created.
    Created.

---
+++ data.java
@@ -0,0 +1,9 @@
+// Data
+
+public class data {
+        public static void main(String[] args) {
+                // Key list (Updated)
+                // Index data
+                String[] keylist = {"key1", "key2", "key3", "key4"};
+        }
+}
