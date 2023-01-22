6d888baf9a9918450cdd24afda4443862465106b
Celvis <78430018+Celvis-wq@users.noreply.github.com>
2023-01-22 16:39:54 -0600

    Idk yet honestly
    doing stuff

--- data.java
+++ data.java
@@ -4,6 +4,11 @@ public class data {
         public static void main(String[] args) {
                 // Key list (Updated)
                 // Index data
-                String[] keylist = {"key1", "key2", "key3", "key4"};
+                String[] keylist = {"key1", "key2", "key3"};
+                
+                // INDEX
+                /*
+                [0], [1], [2]
+                */
         }
 }
