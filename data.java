49da6eb455bde1966e5f9eec2a4f1f1b7b4f96a8
Celvis <78430018+Celvis-wq@users.noreply.github.com>
2023-02-14 21:23:37 -0600

    This will change soon.
    This will change soon.

--- data.java
+++ data.java
@@ -4,11 +4,11 @@ public class data {
         public static void main(String[] args) {
                 // Key list (Updated)
                 // Index data
-                String[] keylist = {"key1", "key2", "key3"};
+                String[] keylist = {"key1", "key2", "key3", "key4"};
                 
                 // INDEX
                 /*
-                [0], [1], [2]
+                [0], [1], [2], [3]
                 */
                 /*
                 if (key.equals(keylist[0]) {
