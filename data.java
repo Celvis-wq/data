71b84cdb4e53f019448c05bda189c3d73c9e08e6
Celvis <78430018+Celvis-wq@users.noreply.github.com>
2023-02-20 22:39:34 -0600

    Changes soon.
    Changes soon.

--- data.java
+++ data.java
@@ -6,7 +6,7 @@ public class data {
                 // Index data
                 String[] keylist = {"key1", "key2", "key3", "key4"};
                 
-                // INDEX
+                // INDEX TODO
                 /*
                 [0], [1], [2], [3]
                 */
